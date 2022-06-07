d4bc62ee54ce4b8dc712687eb0e4829b9f7fea44
Alvaro Ragazzi <95315441+alvaroragazzi@users.noreply.github.com>
2022-06-06 22:07:27 -0300

    Update ServidorHTTP.py

--- ServidorHTTP.py
+++ ServidorHTTP.py
@@ -1,22 +1,27 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
-class Server(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-
-        self.wfile.write(bytes('''
-            <html>
-                <body>
-                    <h1>Olá</h1>
-                </body>
-            </html>
-        ''', "iso-8859-1"))
+import socket
 
 IP = "localhost"
 PORT = 8080
 
-server = HTTPServer((IP, PORT), Server)
-print("Servidor inciado")
-server.serve_forever()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((IP, PORT))
+server.listen(1)
+
+print(f"Servidor iniciado em http://{IP}:{PORT}")
+
+http_response = ("HTTP/1.0 200 OK\n\nOlá").encode("iso-8859-1")
+
+try:
+    while True:    
+        connection, client_address = server.accept()
+
+        request = connection.recv(1024).decode()
+
+        print(f"Requisição recebida:\n{request}")
+
+        connection.sendall(http_response)
+        connection.close()
+except KeyboardInterrupt:
+    print("Desligando servidor...")
+    server.close()
